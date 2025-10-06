473b71f69dde88abff1700b9b548e17e91c0bcd6
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-10-06 11:20:25 +0700

    soal 2

--- pertemuan2.py
+++ pertemuan2.py
@@ -10,3 +10,22 @@ elif percent >= 60 :
     print("Average performance")
 else : 
     print("Need to Improve!")
+
+
+print("find largest of three numbers!")
+
+number1 = int(input("masukkan angka pertama:"))
+number2 = int(input("masukkan angka kedua:"))
+number3 = int(input("masukkan angka ketiga:"))
+
+if (number1 >= number2) and (number1 >= number3):
+    largest = number1
+elif (number2 >= number1) and (number2 >= number3):
+    largest = number2
+else : 
+    largest = number3
+
+print("\nhasil perhitungan")
+print(f"dari numbers {number1}, {number2}, and {number3},")
+print(f"angka yang terbesar adalah : {largest}")
+
